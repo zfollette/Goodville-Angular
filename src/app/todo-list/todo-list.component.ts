3d7655b1874ff54546274e6fced05bf499a3b140
Zach Follette <zfollette@gmail.com>
2018-05-14 03:45:02 -0400

    Duplicate Entry Check
    Added a duplicate entry checking method. Does not let users enter duplicate tasks. Shows a standard javascript alert window.

--- src/app/todo-list/todo-list.component.ts
+++ src/app/todo-list/todo-list.component.ts
@@ -18,11 +18,26 @@ export class TodoListComponent {
     }
     
     addItem() {
-        this.items.push(new item(this.todoAdd, false)); //add new item to end of array
+        if (this.checkItems(this.todoAdd)) {
+            this.items.push(new item(this.todoAdd, false)); //add new item to end of array
+        } else {
+            alert("Task already exists!");
+        }
+        
         this.todoAdd = null; //clear input via two way binding
     }
     
     checkItem(index: number) {
         this.items[index].itemDone = !this.items[index].itemDone; //toggles boolean regardless of value
+    }
+    
+    checkItems(checkDesc : string) {
+        for (var i = 0; i < this.items.length; i++) {
+            if (this.items[i].itemDesc == checkDesc) {
+                return false; //item already exists
+            }
+        }
+        
+        return true;
     }
 }
